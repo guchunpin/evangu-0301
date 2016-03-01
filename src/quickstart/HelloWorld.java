8860b4b117e49a5f574a6177ecff1b5760aa1528
guchunpin <guchunpin@yahoo.com>
2016-03-01 21:54:06 +0800

    Initial commit

---
+++ src/quickstart/HelloWorld.java
@@ -0,0 +1,20 @@
+package quickstart;
+
+/**
+ * Created by evangu on 16/3/1.
+ */
+import java.util.*;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("输入你的分数");
+        Scanner in = new Scanner(System.in);
+        int score = in.nextInt();
+        if (score>90) {
+            System.out.println("Hello,World");
+        }
+        else {
+            System.out.println("shit");
+        }
+    }
+}
