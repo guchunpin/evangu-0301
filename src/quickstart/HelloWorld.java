f956033e03e24b9f7dba97e07887dc9e1c918424
guchunpin <guchunpin@yahoo.com>
2016-03-15 11:58:39 +0800

    20160315

--- src/quickstart/HelloWorld.java
+++ src/quickstart/HelloWorld.java
@@ -7,14 +7,28 @@ import java.util.*;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("输入你的分数");
+        System.out.println("输入数值");
         Scanner in = new Scanner(System.in);
         int score = in.nextInt();
-        if (score>90) {
+        /*if (score>90) {
             System.out.println("Hello,World");
         }
         else {
             System.out.println("shit11");
+        }*/
+        switch (score){
+            case 1:
+                System.out.println("你出的是石头");
+                break;
+            case 2:
+                System.out.println("你出的是剪刀");
+                break;
+            case 3:
+                System.out.println("你出的是布");
+                break;
+            default:
+                System.out.println("请输入正确的数值");
+                break;
         }
     }
 }
